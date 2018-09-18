11c6492ee3b594df05cb86534ed5a085efe4ef9a
sammyukavi <sammyukavi@gmail.com>
2018-09-18 17:08:32 +0300

    Initial Commit Created Gradle Project

---
+++ src/main/java/ke/co/sendy/casestudy/MainApplication.java
@@ -0,0 +1,17 @@
+package ke.co.sendy.casestudy;
+
+/**
+ * This application finds all possible order combination opportunities given the location coordinates for each order
+ */
+public class MainApplication {
+	
+	
+	/**
+	 * This is the main entry point of the application
+	 *
+	 * @param args Any params passed via commandline to the application
+	 */
+	public static void main(String[] args) {
+		System.out.println("Called");
+	}
+}
